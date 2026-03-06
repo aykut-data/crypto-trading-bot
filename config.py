bed6601e9e5d2642da0feabe162482eb28ef2929
Aykut Yilmaz <45981646+aykut-data@users.noreply.github.com>
2026-03-06 19:54:36 +0000

    Add files via upload

---
+++ config.py
@@ -0,0 +1,10 @@
+api_key = "your_binance_api_key_here"
+api_secret = "your_binance_api_secret_here"
+
+## Setup Instructions:
+
+#1. Login to https://www.binance.com
+#2. Go to Account > API Management
+#3. Create API Key 
+#4. Copy API Key and Secret Key to the fields below
+#5. NEVER commit this file to GitHub!
